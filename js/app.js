ba596a8bef84a76ff787e6659ca849c8574f08b3
Yulyashka6 <128845909+Yulyashka6@users.noreply.github.com>
2023-08-18 00:53:21 +0300

    load project

---
+++ js/app.js
@@ -0,0 +1,19 @@
+VANTA.BIRDS({
+  el: "#intro",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  backgroundColor: 0xf9efdb,
+  color1: 0xff5f00,
+  color2: 0x7cff,
+  colorMode: "variance",
+  birdSize: 1.00,
+  wingSpan: 32.00,
+  separation: 40.00,
+  alignment: 28.00,
+  cohesion: 21.00
+})
